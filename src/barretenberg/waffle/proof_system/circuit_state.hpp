a191db390feec4803f0bd1b66b6753e6e2349772
zac-williamson <blorktronics@gmail.com>
2019-10-24 23:19:03 +0100

    Refactored CircuitFFTState's constructors
    `CircuitFFTState` was triggering crashes when creating
    empty `Prover` containers. The default constructor was
    crewating a size-zero evaluation domain, causing issues
    with `evaluation_domain::compute_lookup_table`.
    
    `CircuitFFTState` will now only create lookup tables for its
    evaluation domains, if the domain size is not 0.
    
    In `CircuitFFTState`'s move constructor, the evaluation domains
    were being initialized via their copy constructors, which is now fixed.
    
    Added move assignment operator to `CircuitFFTState` - the move
    semantics for `Prover` should probably work as intended now...

--- src/barretenberg/waffle/proof_system/circuit_state.hpp
+++ src/barretenberg/waffle/proof_system/circuit_state.hpp
@@ -11,19 +11,30 @@ class CircuitFFTState
 public:
     CircuitFFTState(const size_t n) : small_domain(n), mid_domain(2*n), large_domain(4*n)
     {
-        small_domain.compute_lookup_table();
-        mid_domain.compute_lookup_table();
-        large_domain.compute_lookup_table();
+        if (n != 0)
+        {
+            small_domain.compute_lookup_table();
+            mid_domain.compute_lookup_table();
+            large_domain.compute_lookup_table();
+        }
     }
     CircuitFFTState(const CircuitFFTState& other) :
         small_domain(other.small_domain),
         mid_domain(other.mid_domain),
         large_domain(other.large_domain) {}
 
-    CircuitFFTState(CircuitFFTState&& other) :
-        small_domain(other.small_domain),
-        mid_domain(other.mid_domain),
-        large_domain(other.large_domain) {}
+    CircuitFFTState(CircuitFFTState &&other) :
+        small_domain(std::move(other.small_domain)),
+        mid_domain(std::move(other.mid_domain)),
+        large_domain(std::move(other.large_domain)) {}
+
+    CircuitFFTState& operator=(CircuitFFTState &&other)
+    {
+        small_domain = std::move(other.small_domain);
+        mid_domain = std::move(other.mid_domain);
+        large_domain = std::move(other.large_domain);
+        return *this;
+    }
 
     ~CircuitFFTState() {};
 
