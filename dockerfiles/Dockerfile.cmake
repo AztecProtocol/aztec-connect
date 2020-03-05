def286a3264b0c251d9d1aa80a84c7b00c0ef9d0
Charlie Lye <karl.lye@gmail.com>
2020-03-05 17:42:55 +0000

    Cmake to 3.16

--- dockerfiles/Dockerfile.cmake
+++ dockerfiles/Dockerfile.cmake
@@ -1,8 +1,8 @@
 FROM ubuntu:latest
 RUN apt-get update && apt-get install -y build-essential wget git
-RUN wget https://cmake.org/files/v3.15/cmake-3.15.4.tar.gz \
-  && tar zxfv cmake-3.15.4.tar.gz \
-  && cd cmake-3.15.4 \
+RUN wget https://cmake.org/files/v3.15/cmake-3.16.4.tar.gz \
+  && tar zxfv cmake-3.16.4.tar.gz \
+  && cd cmake-3.16.4 \
   && ./bootstrap \
   && make -j8 \
   && make install \
