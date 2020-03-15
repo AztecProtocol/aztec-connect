883f8fcb12a346be354123283ec7e643a04917a4
Charlie Lye <karl.lye@gmail.com>
2020-03-15 17:13:37 +0000

    Inital.

--- hummus/src/index.tsx
+++ hummus/src/index.tsx
@@ -9,9 +9,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BarretenbergWasm } from 'barretenberg/wasm';
 import { Schnorr } from 'barretenberg/crypto/schnorr';
+import { parseAuthData, AuthData } from './parseAuthData';
+import * as cbor from 'cbor';
+import { createHash } from 'crypto';
 require('barretenberg/wasm/barretenberg.wasm');
 
-async function main() {
+async function signAThing() {
   const barretenberg = new BarretenbergWasm();
   await barretenberg.init();
   const schnorr = new Schnorr(barretenberg);
@@ -27,7 +30,146 @@ async function main() {
 
   console.log(verified);
 
-  ReactDOM.render(<p/>, document.getElementById('root'));
+  return verified
+}
+
+async function makeCred(): Promise<AuthData> {
+  const id = Uint8Array.from(window.atob("MIIBkzCCATigAwIBAjCCAZMwggE4oAMCAQIwggGTMII="), c=>c.charCodeAt(0));
+  const challenge = new Uint8Array(32);
+  window.crypto.getRandomValues(challenge);
+  const publicKey: PublicKeyCredentialCreationOptions = {
+    challenge,
+
+    rp: {
+        name: "AZTEC"
+    },
+
+    user: {
+        id,
+        name: "charlie",
+        displayName: "Charlie"
+    },
+
+    authenticatorSelection: {userVerification : 'required'},
+
+    attestation: "none",
+
+    pubKeyCredParams: [
+        {
+            type: "public-key", alg: -7 // "ES256" IANA COSE Algorithms registry
+        },
+        {
+            type: "public-key", alg: -257 // "RS256" IANA COSE Algorithms registry
+        }
+    ]
+  }
+
+  const newCredentialInfo = await navigator.credentials.create({ publicKey }) as PublicKeyCredential;
+  const attestationResponse = newCredentialInfo.response as AuthenticatorAttestationResponse;
+
+  console.log(attestationResponse);
+  // let attestationObjectBuffer = window.atob(attestationResponse.attestationObject);
+  let ctapMakeCredResp        = cbor.decodeAllSync(Buffer.from(attestationResponse.attestationObject))[0];
+  console.log(ctapMakeCredResp);
+
+  return parseAuthData(ctapMakeCredResp.authData);
+}
+
+async function sign(credentialId: Buffer, challenge: Buffer) {
+  var publicKey: PublicKeyCredentialRequestOptions = {
+    challenge,
+    allowCredentials: [
+        { type: "public-key", id: credentialId }
+    ],
+    userVerification : 'required',
+  }
+
+  const getAssertionResponse = await navigator.credentials.get({ publicKey }) as PublicKeyCredential;
+  return getAssertionResponse.response as AuthenticatorAssertionResponse;
+}
+
+interface Creds {
+  counter: number;
+  credId: Buffer;
+  pubKey: Buffer;
+};
+
+function toHexString(byteArray: Uint8Array) {
+  return Array.prototype.map.call(byteArray, function(byte) {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join('');
+}
+function toByteArray(hexString: string) {
+  var result = [];
+  for (var i = 0; i < hexString.length; i += 2) {
+    result.push(parseInt(hexString.substr(i, 2), 16));
+  }
+  return result;
+}
+
+async function doTheThings() {
+  const existingCreds = window.localStorage.getItem('creds');
+  let creds: Creds;
+  if (!existingCreds) {
+    const parsedAuthData = await makeCred();
+    console.log(parsedAuthData);
+
+    // Store counter, credId, and pub key.
+    creds = {
+      counter: parsedAuthData.counter,
+      credId: parsedAuthData.credIdBuffer!,
+      pubKey: parsedAuthData.cosePublicKeyBuffer!,
+    };
+    const toStore = {
+      counter: parsedAuthData.counter,
+      credId: parsedAuthData.credIdBuffer!.toString('base64'),
+      pubKey: parsedAuthData.cosePublicKeyBuffer!.toString('base64'),
+    }
+    window.localStorage.setItem("creds", JSON.stringify(toStore))
+  } else {
+    const { counter, credId, pubKey } = JSON.parse(existingCreds);
+    creds = {
+      counter,
+      credId: Buffer.from(credId, 'base64'),
+      pubKey: Buffer.from(pubKey, 'base64'),
+    }
+  }
+
+  console.log("Creds", creds);
+
+  const challenge = Buffer.from("the note data we want to sign");
+  const signResponse = await sign(creds.credId, challenge);
+  console.log(signResponse);
+
+  const clientDataJson = JSON.parse(new TextDecoder("utf-8").decode(signResponse.clientDataJSON));
+  console.log(clientDataJson);
+  if (!challenge.equals(Buffer.from(clientDataJson.challenge, 'base64'))) {
+    throw "Challenge unequal.";
+  }
+  if (clientDataJson.origin != window.location.origin) {
+    throw "Origin unequal.";
+  }
+  if (clientDataJson.type != "webauthn.get") {
+    throw "Type unequal.";
+  }
+
+  const authData = Buffer.from(signResponse.authenticatorData);
+  console.log("auth data", parseAuthData(authData));
+
+  const clientDataHash = createHash('sha256').update('alice', 'utf8').digest();
+  const signatureBase = Buffer.concat([authData, clientDataHash]);
+}
+
+function LandingPage(props: any) {
+  return (
+    <form>
+      <label>Press the button: </label><input type="button" value="The Button" onClick={doTheThings}></input>
+    </form>
+  );
+}
+
+async function main() {
+  ReactDOM.render(<LandingPage />, document.getElementById('root'));
 }
 
 // tslint:disable-next-line:no-console
