96e07a97798502938a5d589425ce1b5e9a4c7738
Charlie Lye <karl.lye@gmail.com>
2020-04-08 14:26:40 +0100

    Prover logs.

--- barretenberg.js/src/client_proofs/prover/index.ts
+++ barretenberg.js/src/client_proofs/prover/index.ts
@@ -5,23 +5,51 @@ import { Fft } from '../../fft';
 
 const debug = createDebug('bb:prover');
 
+class Timer {
+  private start: number;
+
+  constructor(msg: string) {
+    debug(msg);
+    this.start = new Date().getTime();
+  }
+
+  public mark(msg: string) {
+    const diff = new Date().getTime() - this.start;
+    debug(`${msg} (ms:${diff})`);
+    this.start = new Date().getTime();
+  }
+}
+
 export class Prover {
   constructor(private wasm: BarretenbergWorker, private pippenger: Pippenger, private fft: Fft) {}
 
   public async createProof(proverPtr: number) {
     const circuitSize = await this.wasm.call('prover_get_circuit_size', proverPtr);
+    const timer = new Timer('enter createProof');
     await this.wasm.call('prover_execute_preamble_round', proverPtr);
+    timer.mark('preamble end');
     await this.processProverQueue(proverPtr, circuitSize);
+    timer.mark('first round start');
     await this.wasm.call('prover_execute_first_round', proverPtr);
+    timer.mark('first round end');
     await this.processProverQueue(proverPtr, circuitSize);
+    timer.mark('second round start');
     await this.wasm.call('prover_execute_second_round', proverPtr);
+    timer.mark('second round end');
     await this.processProverQueue(proverPtr, circuitSize);
+    timer.mark('third round start');
     await this.wasm.call('prover_execute_third_round', proverPtr);
+    timer.mark('third round end');
     await this.processProverQueue(proverPtr, circuitSize);
+    timer.mark('fourth round start');
     await this.wasm.call('prover_execute_fourth_round', proverPtr);
+    timer.mark('fourth round end');
     await this.processProverQueue(proverPtr, circuitSize);
+    timer.mark('fifth round start');
     await this.wasm.call('prover_execute_fifth_round', proverPtr);
+    timer.mark('fifth round end');
     await this.processProverQueue(proverPtr, circuitSize);
+    timer.mark('done');
     const proofSize = await this.wasm.call('prover_export_proof', proverPtr, 0);
     const proofPtr = Buffer.from(await this.wasm.sliceMemory(0, 4)).readUInt32LE(0);
     return Buffer.from(await this.wasm.sliceMemory(proofPtr, proofPtr + proofSize));
@@ -34,7 +62,7 @@ export class Prover {
     const fftJobs = jobInfo.readUInt32LE(4);
     const ifftJobs = jobInfo.readUInt32LE(8);
 
-    debug(`scalars:${scalarJobs} ffts:${fftJobs} iffts:${ifftJobs}`);
+    debug(`starting jobs scalars:${scalarJobs} ffts:${fftJobs} iffts:${ifftJobs}`);
 
     for (let i = 0; i < scalarJobs; ++i) {
       const scalarsPtr = await this.wasm.call('prover_get_scalar_multiplication_data', proverPtr, i);
