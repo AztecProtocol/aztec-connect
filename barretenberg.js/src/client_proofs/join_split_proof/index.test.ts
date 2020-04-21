f5ee54c6a282b08fcaa2836e2bf16fcdfdfb90fb
Charlie Lye <karl.lye@gmail.com>
2020-04-21 13:11:09 +0100

    barretenberg.js join split proof test passes.

--- barretenberg.js/src/client_proofs/join_split_proof/index.test.ts
+++ barretenberg.js/src/client_proofs/join_split_proof/index.test.ts
@@ -1,4 +1,4 @@
-import { JoinSplitProver } from './index';
+import { JoinSplitProver, JoinSplitVerifier } from './index';
 import { Schnorr } from '../../crypto/schnorr';
 import createDebug from 'debug';
 import { BarretenbergWasm } from '../../wasm';
@@ -9,26 +9,62 @@ import memdown from 'memdown';
 import { Blake2s } from '../../crypto/blake2s';
 import { Pedersen } from '../../crypto/pedersen';
 import { Note } from '../note';
+import { EventEmitter } from 'events';
+import { Crs } from '../../crs';
+import { WorkerPool } from '../../wasm/worker_pool';
+import { PooledPippenger } from '../../pippenger';
+import { PooledFft } from '../../fft';
+import { Prover } from '../prover';
 
 const debug = createDebug('bb:join_split_proof');
 
 describe('join_split_proof', () => {
   let barretenberg!: BarretenbergWasm;
+  let pool!: WorkerPool;
   let joinSplitProver!: JoinSplitProver;
-  // let createNoteVerifier!: CreateNoteVerifier;
+  let joinSplitVerifier!: JoinSplitVerifier;
   let schnorr!: Schnorr;
   let blake2s!: Blake2s;
   let pedersen!: Pedersen;
 
   beforeAll(async () => {
+    EventEmitter.defaultMaxListeners = 32;
+    const circuitSize = 128 * 1024;
+
+    const crs = new Crs(circuitSize);
+    await crs.download();
+
     barretenberg = await BarretenbergWasm.new();
-    joinSplitProver = new JoinSplitProver(barretenberg);
+
+    pool = new WorkerPool();
+    await pool.init(barretenberg.module, Math.min(navigator.hardwareConcurrency, 8));
+
+    const pippenger = new PooledPippenger();
+    await pippenger.init(crs.getData(), pool);
+
+    const fft = new PooledFft(pool);
+    await fft.init(circuitSize);
+
+    const prover = new Prover(pool.workers[0], pippenger, fft);
+
+    joinSplitProver = new JoinSplitProver(pool.workers[0], prover);
+    joinSplitVerifier = new JoinSplitVerifier(pippenger.pool[0]);
     blake2s = new Blake2s(barretenberg);
     pedersen = new Pedersen(barretenberg);
     schnorr = new Schnorr(barretenberg);
+
+    debug('creating keys...');
+    const start = new Date().getTime();
+    await joinSplitProver.init();
+    await joinSplitVerifier.init(crs.getG2Data());
+    debug(`created circuit keys: ${new Date().getTime() - start}ms`);
+  }, 60000);
+
+  afterAll(async () => {
+    await pool.destroy();
   });
 
-  it('should do something', async () => {
+  it('should construct join split proof', async () => {
     // prettier-ignore
     const pk = Buffer.from([
       0x0b, 0x9b, 0x3a, 0xde, 0xe6, 0xb3, 0xd8, 0x1b, 0x28, 0xa0, 0x88, 0x6b, 0x2a, 0x84, 0x15, 0xc7,
@@ -44,8 +80,8 @@ describe('join_split_proof', () => {
     const outputNote1 = new Note(pubKey, viewingKey, 70);
     const outputNote2 = new Note(pubKey, viewingKey, 80);
 
-    const inputNote1Enc = joinSplitProver.encryptNote(inputNote1);
-    const inputNote2Enc = joinSplitProver.encryptNote(inputNote2);
+    const inputNote1Enc = await joinSplitProver.encryptNote(inputNote1);
+    const inputNote2Enc = await joinSplitProver.encryptNote(inputNote2);
     const tree = new MerkleTree(levelup(memdown()), pedersen, blake2s, 'data', 32);
     await tree.updateElement(0, inputNote1Enc);
     await tree.updateElement(1, inputNote2Enc);
@@ -53,12 +89,12 @@ describe('join_split_proof', () => {
     const inputNote1Path = await tree.getHashPath(0);
     const inputNote2Path = await tree.getHashPath(1);
 
-    const signature = joinSplitProver.sign4Notes([inputNote1, inputNote2, outputNote1, outputNote2], pk);
+    const signature = await joinSplitProver.sign4Notes([inputNote1, inputNote2, outputNote1, outputNote2], pk);
 
     const tx = new JoinSplitTx(
       pubKey,
-      10,
-      20,
+      0,
+      0,
       2,
       [0, 1],
       tree.getRoot(),
@@ -68,6 +104,15 @@ describe('join_split_proof', () => {
       signature,
     );
 
-    await joinSplitProver.createJoinSplitProof(tx);
-  });
+    debug('creating proof...');
+    const start = new Date().getTime();
+    const proof = await joinSplitProver.createJoinSplitProof(tx);
+    debug(`created proof: ${new Date().getTime() - start}ms`);
+    debug(`proof size: ${proof.length}`);
+
+    const verified = await joinSplitVerifier.verifyProof(proof);
+    expect(verified).toBe(true);
+
+    debug(`mem: ${await barretenberg.getMemory().length}`);
+  }, 60000);
 });
--- barretenberg.js/src/client_proofs/join_split_proof/index.ts
+++ barretenberg.js/src/client_proofs/join_split_proof/index.ts
@@ -2,44 +2,43 @@ import { BarretenbergWorker } from '../../wasm/worker';
 import { Prover } from '../prover';
 import { SinglePippenger } from '../../pippenger';
 import { JoinSplitTx } from './join_split_tx';
-import { BarretenbergWasm } from '../../wasm';
 import { Note } from '../note';
 import { Signature } from '../signature';
 
 export class JoinSplitProver {
-  constructor(private wasm: BarretenbergWasm/*, private prover: Prover*/) {}
+  constructor(private wasm: BarretenbergWorker, private prover: Prover) {}
 
   public init() {
     this.wasm.call('join_split__init_proving_key');
   }
 
-  public encryptNote(note: Note) {
-    this.wasm.transferToHeap(note.toBuffer(), 0);
-    this.wasm.call('join_split__encrypt_note', 0, 100);
-    return Buffer.from(this.wasm.sliceMemory(100, 164));
+  public async encryptNote(note: Note) {
+    await this.wasm.transferToHeap(note.toBuffer(), 0);
+    await this.wasm.call('join_split__encrypt_note', 0, 100);
+    return Buffer.from(await this.wasm.sliceMemory(100, 164));
   }
 
-  public sign4Notes(notes: Note[], pk: Buffer) {
+  public async sign4Notes(notes: Note[], pk: Buffer) {
     const buf = Buffer.concat(notes.map(n => n.toBuffer()));
-    this.wasm.transferToHeap(pk, 0);
-    this.wasm.transferToHeap(buf, 32);
-    this.wasm.call('join_split__sign_4_notes', 32, 0, 0);
-    const sig = Buffer.from(this.wasm.sliceMemory(0, 64));
+    await this.wasm.transferToHeap(pk, 0);
+    await this.wasm.transferToHeap(buf, 32);
+    await this.wasm.call('join_split__sign_4_notes', 32, 0, 0);
+    const sig = Buffer.from(await this.wasm.sliceMemory(0, 64));
     return new Signature(sig.slice(0, 32), sig.slice(32, 64));
   }
 
   public async createJoinSplitProof(tx: JoinSplitTx) {
     const buf = tx.toBuffer();
-    const mem = this.wasm.call('bbmalloc', buf.length);
-    this.wasm.transferToHeap(buf, mem);
-    const proverPtr = this.wasm.call('join_split__new_prover', mem, buf.length);
-    this.wasm.call('bbfree', mem);
-    // const proof = await this.prover.createProof(proverPtr);
+    const mem = await this.wasm.call('bbmalloc', buf.length);
+    await this.wasm.transferToHeap(buf, mem);
+    const proverPtr = await this.wasm.call('join_split__new_prover', mem, buf.length);
+    await this.wasm.call('bbfree', mem);
+    const proof = await this.prover.createProof(proverPtr);
     await this.wasm.call('create_note__delete_prover', proverPtr);
-    return Buffer.alloc(0);
+    return proof;
   }
 }
-/*
+
 export class JoinSplitVerifier {
   private wasm: BarretenbergWorker;
 
@@ -49,15 +48,14 @@ export class JoinSplitVerifier {
 
   public async init(g2Data: Uint8Array) {
     await this.wasm.transferToHeap(g2Data, 0);
-    await this.wasm.call('create_note__init_verification_key', this.pippenger.getPointer(), 0);
+    await this.wasm.call('join_split__init_verification_key', this.pippenger.getPointer(), 0);
   }
 
   public async verifyProof(proof: Buffer) {
     const proofPtr = await this.wasm.call('bbmalloc', proof.length);
     await this.wasm.transferToHeap(proof, proofPtr);
-    const verified = (await this.wasm.call('create_note__verify_proof', proofPtr, proof.length)) ? true : false;
+    const verified = (await this.wasm.call('join_split__verify_proof', proofPtr, proof.length)) ? true : false;
     await this.wasm.call('bbfree', proofPtr);
     return verified;
   }
 }
-*/
