f3bed610604d1b3a1cfa15d132f971733c1de3a1
Leila Wang <leizciw@gmail.com>
2020-09-22 15:58:11 +0100

    Adjust Signer interface.

--- sdk/src/signer/index.ts
+++ sdk/src/signer/index.ts
@@ -1,4 +1,7 @@
+import { TransactionRequest, TransactionResponse } from '@ethersproject/abstract-provider';
+
 export interface Signer {
   getAddress(): Promise<string>;
   signMessage(message: string | Buffer): Promise<string>;
+  sendTransaction?(tx: TransactionRequest): Promise<TransactionResponse>;
 }
