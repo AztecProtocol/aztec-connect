d0a0e4a3c0f779df5bef0d4925acd19823c671b4
Leila Wang <leizciw@gmail.com>
2020-09-22 16:55:11 +0100

    Remove redundant file.

--- sdk/src/signer.ts
+++
@@ -1,8 +0,0 @@
-import { Provider, TransactionRequest, TransactionResponse } from '@ethersproject/abstract-provider';
-
-export interface Signer {
-  provider?: Provider;
-  getAddress(): Promise<string>;
-  signMessage(message: string | Buffer): Promise<string>;
-  sendTransaction(tx: TransactionRequest): Promise<TransactionResponse>;
-}
