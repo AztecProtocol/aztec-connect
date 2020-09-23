16bccac65b13cab0487857f4653a67a6acbe3003
Leila Wang <leizciw@gmail.com>
2020-09-23 17:40:22 +0100

    Remove unused method from eth Signer interface.

--- sdk/src/signer/index.ts
+++ sdk/src/signer/index.ts
@@ -1,7 +1,4 @@
-import { TransactionRequest, TransactionResponse } from '@ethersproject/abstract-provider';
-
 export interface Signer {
   getAddress(): Promise<string>;
   signMessage(message: string | Buffer): Promise<string>;
-  sendTransaction?(tx: TransactionRequest): Promise<TransactionResponse>;
 }
