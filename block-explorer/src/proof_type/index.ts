ca3f981c051e84ea7324f4d52afc5ad80b5967c3
Leila Wang <leizciw@gmail.com>
2020-10-02 17:22:23 +0100

    Fix proof id to type mappings.

--- block-explorer/src/proof_type/index.ts
+++ block-explorer/src/proof_type/index.ts
@@ -5,5 +5,5 @@ export type ProofId = number;
 export type ProofType = 'ACCOUNT' | 'JOIN_SPLIT';
 
 export const proofIdToType = (proofId: ProofId) => {
-  return proofId === 1 ? 'JOIN_SPLIT' : 'ACCOUNT';
+  return proofId === 1 ? 'ACCOUNT' : 'JOIN_SPLIT';
 };
