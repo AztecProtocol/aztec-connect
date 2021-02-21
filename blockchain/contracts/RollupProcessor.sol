f5e2d6300c3cb9466c61d7d52b948f5f67e099bb
Charlie Lye <karl.lye@gmail.com>
2021-02-21 00:08:36 +0000

    Make contracts redploy due to viewing key size changes.

--- blockchain/contracts/RollupProcessor.sol
+++ blockchain/contracts/RollupProcessor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// Copyright 2020 Spilsbury Holdings Ltd
+// Copyright 2020 Spilsbury Holdings Ltd.
 pragma solidity >=0.6.10 <0.8.0;
 
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
