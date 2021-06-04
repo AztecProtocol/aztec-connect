91a10e0b9e387ec28e99d5aaa3c2a2d18b5398c2
Leila Wang <leizciw@gmail.com>
2021-06-04 12:09:40 +0100

    Claim note apis. (#375)
    * Export wasm fn to encrypt claim note and compute nullifier.
    
    * Add claim note apis to note algorithms.

--- barretenberg.js/src/client_proofs/note_algorithms/index.test.ts
+++ barretenberg.js/src/client_proofs/note_algorithms/index.test.ts
@@ -3,26 +3,27 @@ import { Grumpkin } from '../../ecc/grumpkin';
 import { GrumpkinAddress } from '../../address';
 import { NoteAlgorithms } from '.';
 import { TreeNote } from '../tree_note';
+import { TreeClaimNote } from '../tree_claim_note';
 // import createDebug from 'debug';
 
 // const debug = createDebug('bb:decrypt_test');
 
 describe('compute_nullifier', () => {
-  let barretenberg!: BarretenbergWasm;
   let grumpkin!: Grumpkin;
   let noteAlgos!: NoteAlgorithms;
+  let pubKey: GrumpkinAddress;
 
   const privateKey = Buffer.from('0b9b3adee6b3d81b28a0886b2a8415c7da31291a5e96bb7a56639e177d301beb', 'hex');
   const noteSecret = Buffer.from('0000000011111111000000001111111100000000111111110000000011111111', 'hex');
 
   beforeAll(async () => {
-    barretenberg = await BarretenbergWasm.new();
+    const barretenberg = await BarretenbergWasm.new();
     grumpkin = new Grumpkin(barretenberg);
     noteAlgos = new NoteAlgorithms(barretenberg);
+    pubKey = new GrumpkinAddress(grumpkin.mul(Grumpkin.one, privateKey));
   });
 
   it('should compute correct nullifier', async () => {
-    const pubKey = new GrumpkinAddress(grumpkin.mul(Grumpkin.one, privateKey));
     const inputNote1 = new TreeNote(pubKey, BigInt(100), 0, 0, noteSecret);
     const inputNote2 = new TreeNote(pubKey, BigInt(50), 0, 0, noteSecret);
     inputNote1.noteSecret = noteSecret;
@@ -39,5 +40,12 @@ describe('compute_nullifier', () => {
 
     expect(nullifier1.toString('hex')).toEqual(expected1);
     expect(nullifier2.toString('hex')).toEqual(expected2);
+  });
+
+  it('should encrypt claim note and compute its nullifier', async () => {
+    const inputNote = new TreeClaimNote(BigInt(100), BigInt(234), noteSecret, 0);
+    const inputNoteEnc = noteAlgos.encryptClaimNote(inputNote.toBuffer(), pubKey, 0);
+    const nullifier = noteAlgos.computeClaimNoteNullifier(inputNoteEnc, 1);
+    expect(nullifier).toEqual(Buffer.from('06c772265c0e55bd0b15ec6fe660926161954775a52926457a44e1eafe501e03', 'hex'));
   });
 });
