71d0bb3cbf70eb39f2e46f04885390727f0f3f7a
Charlie Lye <karl.lye@gmail.com>
2021-11-09 15:55:16 +0000

    Fix create proof scripts.

--- blockchain/src/contracts/verifier/fixtures/create_proofs_dev.sh
+++ blockchain/src/contracts/verifier/fixtures/create_proofs_dev.sh
@@ -6,6 +6,5 @@ rm -rf build-vks && mkdir build-vks && cd build-vks && cmake ..
 cd ../../blockchain/src/contracts/verifier/fixtures
 
 # input format:
-# num_txs | split proofs ? | data_dir | inner size | outer size |
-#   | valid outer sizes described as in 1xa,1xb,1xc |
-./create_rollup_proof.sh 1 0 ./data 1 2 1x2
+# num_txs | split proofs ? | data_dir | inner size | outer size | valid outer sizes |
+./create_rollup_proof.sh 1 0 ./data 1 2 2
--- blockchain/src/contracts/verifier/fixtures/create_proofs_prod.sh
+++ blockchain/src/contracts/verifier/fixtures/create_proofs_prod.sh
@@ -2,10 +2,8 @@
 set -e
 rm -rf ../../../../../barretenberg/build/data
 
-# input format: 
-# num_txs | split proofs ? | data_dir | inner size | outer size | 
-#   | valid outer sizes described as in 28xa,28xb,28xc |
-
-./create_rollup_proof.sh 1 0 ./data 28 1 28x1,28x2,28x4
-./create_rollup_proof.sh 1 0 ./data 28 2 28x1,28x2,28x4
-./create_rollup_proof.sh 1 0 ./data 28 4 28x1,28x2,28x4
+# input format:
+# num_txs | split proofs ? | data_dir | inner size | outer size | valid outer sizes |
+./create_rollup_proof.sh 1 0 ./data 28 1 1,2,4
+./create_rollup_proof.sh 1 0 ./data 28 2 1,2,4
+./create_rollup_proof.sh 1 0 ./data 28 4 1,2,4
