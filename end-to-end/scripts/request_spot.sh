96f07310fb550270c939b7690035539b50de34bd
Charlie Lye <karl.lye@gmail.com>
2021-11-09 21:46:55 +0000

    Fall back to ondemand instances when no spot after 60s.

--- end-to-end/scripts/request_spot.sh
+++ end-to-end/scripts/request_spot.sh
@@ -3,9 +3,10 @@ set -e
 
 NAME=$1
 
+export AWS_DEFAULT_REGION=us-east-2
+
 >&2 echo "Requesting instance..."
 SIR=$(aws ec2 request-spot-instances \
-  --region us-east-2 \
   --spot-price "0.80" \
   --instance-count 1 \
   --type "one-time" \
@@ -14,20 +15,30 @@ SIR=$(aws ec2 request-spot-instances \
   --output text)
 
 >&2 echo "Waiting for instance id for spot request: $SIR..."
-while [ -z "$IID" -o "$IID" == "None" ]; do
+for i in {1..12}; do
   IID=$(aws ec2 describe-spot-instance-requests \
-    --region us-east-2 \
     --spot-instance-request-ids $SIR \
     --query "SpotInstanceRequests[*].[InstanceId]" \
     --output text)
+  [ -z "$IID" -o "$IID" == "None" ] || break
   sleep 5
 done
 
-aws ec2 create-tags --region us-east-2 --resources $IID --tags "Key=Name,Value=$NAME"
-aws ec2 create-tags --region us-east-2 --resources $IID --tags "Key=Group,Value=build-instance"
+if [ -z "$IID" -o "$IID" == "None" ]; then
+  # Cancel spot request.
+  aws ec2 cancel-spot-instance-requests --spot-instance-request-ids $SIR > /dev/null
+  >&2 echo "Falling back to on-demand instance..."
+  # Request on-demand instance.
+  IID=$(aws ec2 run-instances \
+    --cli-input-json file://specification.json \
+    --query "Instances[*].[InstanceId]" \
+    --output text)
+fi
+
+aws ec2 create-tags --resources $IID --tags "Key=Name,Value=$NAME"
+aws ec2 create-tags --resources $IID --tags "Key=Group,Value=build-instance"
 
 aws ec2 describe-instances \
-  --region us-east-2 \
   --filter "Name=instance-id,Values=$IID" \
   --query "Reservations[*].Instances[*].PublicIpAddress" \
   --output=text
