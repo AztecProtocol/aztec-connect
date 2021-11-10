a51c6f11cbb45088f8325dc4247e1944c37e965c
Innokentii Sennovskii <isennovskiy@gmail.com>
2021-11-10 17:14:17 +0300

    A port of security checks for the bigfield class (#532)
    * Co-authored-by: Zachary James Williamson <zac-williamson@users.noreply.github.com>
    Co-authored-by: Arijit Dutta <arijitdutta67@users.noreply.github.com>
    
    * Added mult_madd. Renamed internal functions to unsafe_*
    
    * Changed dual_madd to proxy to mult_madd
    
    * Rewrote msub_div to proxy to mult_madd
    
    * Resolved bridge_id conflict with defi
    
    * Changed the failing test, since we have an assert now
    
    * Fixed some maximum_values I missed
    
    * Added logic of fixing witnesses
    
    Co-authored-by: Ariel <arielgabizon@users.noreply.github.com>
    Co-authored-by: Zachary James Williamson <zac-williamson@users.noreply.github.com>
    
    * Updated fixtures
    
    * Changed uintx
    
    * Disabled reduce_buckets_basic test
    
    * Refactored division
    
    * Updated blockchain data
    
    * Removed unneeded verifier keys
    
    * Changed initialization
    
    Co-authored-by: Zachary James Williamson <blorktronics@gmail.com>
    Co-authored-by: Ariel <arielgabizon@users.noreply.github.com>
    Co-authored-by: Zachary James Williamson <zac-williamson@users.noreply.github.com>

--- blockchain/contracts/verifier/keys/RootVerifierVk.sol
+++ blockchain/contracts/verifier/keys/RootVerifierVk.sol
@@ -1,4 +1,4 @@
-// Verification Key Hash: 94fd851d54e14f15a7508a0d3791d39258e66f7bc08654b73f0115e5920593f9
+// Verification Key Hash: 5e20d24a32e6466e3f9c1775b0f92956b932b6e884788bca718161f84b7be29b
 // SPDX-License-Identifier: GPL-2.0-only
 // Copyright 2021 Spilsbury Holdings Ltd
 
@@ -21,22 +21,22 @@ library RootVerifierVk {
             mstore(add(vk, 0x40),0x0210fe635ab4c74d6b7bcf70bc23a1395680c64022dd991fb54d4506ab80c59d) // vk.work_root
             mstore(add(vk, 0x60),0x30644e121894ba67550ff245e0f5eb5a25832df811e8df9dd100d30c2c14d821) // vk.domain_inverse
             mstore(add(vk, 0x80),0x2165a1a5bda6792b1dd75c9f4e2b8e61126a786ba1a6eadf811b03e7d69ca83b) // vk.work_root_inverse
-            mstore(mload(add(vk, 0xa0)), 0x1ca075d84340b9335c97fccf2ca37d68243fbfd90a6b4a1dacdecfb9a2f4ce95)//vk.Q1
-            mstore(add(mload(add(vk, 0xa0)), 0x20), 0x00be9ff7fa92054c1dfaa1c3040c455bd3d60a7e8c78eb4df6b44889872d2bec)
-            mstore(mload(add(vk, 0xc0)), 0x1ccff1fc8c9896d2bcc6ed863a28e16165cb9a62fefc29ea32a16913a69f9b3d)//vk.Q2
-            mstore(add(mload(add(vk, 0xc0)), 0x20), 0x1cedfd1c860eee01bcf69cd81bb7f17b8691e2e8e4fdbfc2e94a823d368cb5e1)
-            mstore(mload(add(vk, 0xe0)), 0x2d375901c95631bc1ebdbe21192ed76ab86532b8032f66795113dff457d81a6f)//vk.Q3
-            mstore(add(mload(add(vk, 0xe0)), 0x20), 0x26e3d047035b711516a52932cfd2a28e5bff9efe54cb19f78a395288d39f2dd6)
-            mstore(mload(add(vk, 0x100)), 0x105a311dd6cee686297720e74e73cb3fe884e43572f4d10299732b6eba44e929)//vk.QM
-            mstore(add(mload(add(vk, 0x100)), 0x20), 0x03c5dd2dd7ac63bc8900def2f3611fb83d471293877e739139dd3dfa1e5b6505)
-            mstore(mload(add(vk, 0x120)), 0x11414d0e99a17d90d59f79c83719d5a2ea584b031b1501aadae66ac0f6653b41)//vk.QC
-            mstore(add(mload(add(vk, 0x120)), 0x20), 0x1ddb3519a9313127df8af34490347fcad23c65e4e0084a17c9e7f5d9d8938fb3)
-            mstore(mload(add(vk, 0x140)), 0x21c29d2c192d515d1c5f4edd9393ff1eadbdb7083c379272ab0213eca179de2b)//vk.SIGMA1
-            mstore(add(mload(add(vk, 0x140)), 0x20), 0x05dd8ab87bc9b9003a901995a4d3fa487d7c2b70b5f6138e0dd55f8354128c10)
-            mstore(mload(add(vk, 0x160)), 0x18fd174952e52225611e6b0c5fe9c3b4b88e49df4dad8a405a6c8c9fe3505cb5)//vk.SIGMA2
-            mstore(add(mload(add(vk, 0x160)), 0x20), 0x11126d1ab8b8d9063c85f89de638069be4e7df303f08cc7e9f3cc1761e730274)
-            mstore(mload(add(vk, 0x180)), 0x2487cf84d911ec4514224eb647b7b38994884988c691db727fd8b4511711558a)//vk.SIGMA3
-            mstore(add(mload(add(vk, 0x180)), 0x20), 0x13744a193dff2b903d47c59d717286fc1d46bdbe441b9c99bae9b01212857230)
+            mstore(mload(add(vk, 0xa0)), 0x0cdf188de7b607fc71f27acf2cf3b09f04f6513e68e7c15aecdcaf3a59d494f8)//vk.Q1
+            mstore(add(mload(add(vk, 0xa0)), 0x20), 0x1160a977c7fcd64b7a1d843604c1ad1bafa9fabac35704bad44a452a31d8eeae)
+            mstore(mload(add(vk, 0xc0)), 0x0aa45508ba95d349d425478603224bcbe41c9c3c4b96cd43c865f3fc345ce1d0)//vk.Q2
+            mstore(add(mload(add(vk, 0xc0)), 0x20), 0x2b1563f06787d9fc7e6a3a9fe65d06e6025df001d8cf76f6d4a74cb98ffe572c)
+            mstore(mload(add(vk, 0xe0)), 0x21a15a83f1ece891b6e0478cd28b5225ea4051b9a1668cbeed2ea1701874e70c)//vk.Q3
+            mstore(add(mload(add(vk, 0xe0)), 0x20), 0x22ab06fffbf360ef54139a3c666c85387c36c49b207ca3f4d72ec06fb3efa110)
+            mstore(mload(add(vk, 0x100)), 0x0ad4ff8e0acd928bb6bf3a9f4756f4fa7d142c37f1a0f4f87f7219638a2a1da4)//vk.QM
+            mstore(add(mload(add(vk, 0x100)), 0x20), 0x01ba625d1330c9a17d1430b54bdb0dfa78322933ac93f17709ae50fa485a3589)
+            mstore(mload(add(vk, 0x120)), 0x221cb4832fdf22d26e448fff08844566c36897e68b23d95db57fcbdea86ebbf3)//vk.QC
+            mstore(add(mload(add(vk, 0x120)), 0x20), 0x1fe6c18953d0005ae6e9c32cafbf5e8170db1c48c7eebe9921e0dd0355dd707f)
+            mstore(mload(add(vk, 0x140)), 0x1bfa41ce8eb2c653176b226e32e70e4730e7c8b683aaafdac69de8b322dfbc56)//vk.SIGMA1
+            mstore(add(mload(add(vk, 0x140)), 0x20), 0x0830161a7f0b5863d10cb63c6a6ecc909365640a3ac14b7b37b814246877f2cd)
+            mstore(mload(add(vk, 0x160)), 0x2949aa9501674b2a964d74b68f41210d78cde24c6758e2e69b0bb79471599cdc)//vk.SIGMA2
+            mstore(add(mload(add(vk, 0x160)), 0x20), 0x2bc8de2cf858e33ccd2f97972af09e9a2d62c6988f89229e8c58bf7cffaaf520)
+            mstore(mload(add(vk, 0x180)), 0x1a15564a2d13c54477cc875f39be55006221a8943243d02a547ed0245d1bfa74)//vk.SIGMA3
+            mstore(add(mload(add(vk, 0x180)), 0x20), 0x06c756095d659178c9a0067a4f97dd3aad6c60589dca1415456731b5dfb07e71)
             mstore(add(vk, 0x1a0), 0x01) // vk.contains_recursive_proof
             mstore(add(vk, 0x1c0), 1) // vk.recursive_proof_public_input_indices
             mstore(mload(add(vk, 0x1e0)), 0x260e01b251f6f1c7e7ff4e580791dee8ea51d87a358e038b4efe30fac09383c1) // vk.g2_x.X.c1
