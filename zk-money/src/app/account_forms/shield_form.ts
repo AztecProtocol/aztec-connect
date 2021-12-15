2256a7697a534344d60f0c91c50c7b8da607d6eb
jcf120 <jossie.cliffordfrith@gmail.com>
2021-12-15 15:46:28 +0000

    Rounding on gas fee message

--- zk-money/src/app/account_forms/shield_form.ts
+++ zk-money/src/app/account_forms/shield_form.ts
@@ -482,9 +482,10 @@ export class ShieldForm extends EventEmitter implements AccountForm {
       } else if (amountValue > maxAmount.value) {
         toUpdate.amount = withError(
           amountInput,
-          `Insufficient ${this.asset.symbol} Balance. Please reserve at least ${fromBaseUnits(
+          `Insufficient ${this.asset.symbol} Balance. Please reserve at least ${formatBaseUnits(
             gasCost!.value,
             this.asset.decimals,
+            { precision: preferredFractionalDigits },
           )} ${this.asset.symbol} for gas cost.`,
         );
       }
