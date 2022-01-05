64c75bb764fc3718023fbaf86c5ca7a48c0a350f
jonybur <jobur93@gmail.com>
2022-01-05 19:42:38 +0000

    Refactor shield_prompt, componentise messages

--- zk-money/src/views/account/shield_prompt.tsx
+++ zk-money/src/views/account/shield_prompt.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Asset, formatBaseUnits } from '../../app';
+import { PendingBalance } from '../../app/eth_account/pending_balance';
 import { Button, Text } from '../../components';
 import { breakpoints, spacings } from '../../styles';
 
@@ -31,9 +32,22 @@ const ButtonRoot = styled.div`
 interface ShieldPromptProps {
   asset: Asset;
   balance: bigint;
-  pendingBalance: bigint;
   pendingValue: bigint;
+  pendingBalance: bigint;
   onSubmit: () => void;
+}
+
+interface PendingBalanceMessageProps {
+  pendingBalance: bigint;
+  asset: Asset;
+}
+
+interface ShieldMoreMessageProps {
+  asset: Asset;
+}
+
+interface NoBalanceMessageProps {
+  asset: Asset;
 }
 
 export const ShieldPrompt: React.FunctionComponent<ShieldPromptProps> = ({
@@ -43,37 +57,61 @@ export const ShieldPrompt: React.FunctionComponent<ShieldPromptProps> = ({
   pendingValue = 0n,
   onSubmit,
 }) => {
+  const isBalancePending = pendingBalance > 0n;
+  const hasBalance = balance > 0n;
+  const hasBalancePendingRollup = pendingValue > 0n;
+  const isAllBalancePendingRollup = pendingValue < 0n && balance + pendingValue === 0n;
+
+  let message;
+  if (isBalancePending) {
+    message = <PendingBalanceMessage pendingBalance={pendingBalance} asset={asset} />;
+  } else if ((hasBalance || hasBalancePendingRollup) && !isAllBalancePendingRollup) {
+    message = <ShieldMoreMessage asset={asset} />;
+  } else {
+    message = <NoBalanceMessage asset={asset} />;
+  }
+
   return (
     <Root>
-      <Message size="m">
-        {pendingBalance > 0n ? (
-          <>
-            {'You have '}
-            <Text
-              text={`${formatBaseUnits(pendingBalance, asset.decimals, {
-                precision: asset.preferredFractionalDigits,
-                commaSeparated: true,
-              })} ${asset.symbol}`}
-              weight="bold"
-              inline
-            />
-            {` pending, shield to get started!`}
-          </>
-        ) : (balance > 0n || pendingValue > 0n) && balance - pendingValue !== 0n ? (
-          <>
-            Add to your <Text text={`zk${asset.symbol}`} weight="bold" inline /> by shielding more {asset.symbol}.
-          </>
-        ) : (
-          <>
-            {'You don’t have any '}
-            <Text text={`zk${asset.symbol}`} weight="bold" inline />
-            {`, shield ${asset.symbol} to get started!`}
-          </>
-        )}
-      </Message>
+      <Message size="m">{message}</Message>
       <ButtonRoot>
         <Button theme="gradient" text="Shield" onClick={onSubmit} />
       </ButtonRoot>
     </Root>
+  );
+};
+
+const PendingBalanceMessage: React.FunctionComponent<PendingBalanceMessageProps> = ({ pendingBalance, asset }) => {
+  return (
+    <>
+      {'You have '}
+      <Text
+        text={`${formatBaseUnits(pendingBalance, asset.decimals, {
+          precision: asset.preferredFractionalDigits,
+          commaSeparated: true,
+        })} ${asset.symbol}`}
+        weight="bold"
+        inline
+      />
+      {` pending, shield to get started!`}
+    </>
+  );
+};
+
+const ShieldMoreMessage: React.FunctionComponent<ShieldMoreMessageProps> = ({ asset }) => {
+  return (
+    <>
+      Add to your <Text text={`zk${asset.symbol}`} weight="bold" inline /> by shielding more {asset.symbol}.
+    </>
+  );
+};
+
+const NoBalanceMessage: React.FunctionComponent<NoBalanceMessageProps> = ({ asset }) => {
+  return (
+    <>
+      {'You don’t have any '}
+      <Text text={`zk${asset.symbol}`} weight="bold" inline />
+      {`, shield ${asset.symbol} to get started!`}
+    </>
   );
 };
