4364db2e88e4531f6936da92d83075ddd01fc2b6
Charlie Lye <karl.lye@gmail.com>
2022-02-08 21:32:56 +0000

    Fix

--- falafel/scripts/start_e2e.sh
+++ falafel/scripts/start_e2e.sh
@@ -12,7 +12,7 @@ export HALLOUMI_HOST=http://localhost:8083
 
 export ESCAPE_BLOCK_LOWER=10
 export ESCAPE_BLOCK_UPPER=100
-export VK=MockVerificationKey3x2
+export VK=MockVerificationKey
 
 export MIN_CONFIRMATION_ESCAPE_HATCH_WINDOW=1
 export BASE_TX_GAS=10000
