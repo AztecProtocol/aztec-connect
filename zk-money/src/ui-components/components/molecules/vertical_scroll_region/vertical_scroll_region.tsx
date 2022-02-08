228d336a82ed51c3e5af7d45351d22915689a5cc
jcf120 <jossie.cliffordfrith@gmail.com>
2022-02-08 08:56:18 +0000

    Add mask fade

--- zk-money/src/ui-components/components/molecules/vertical_scroll_region/vertical_scroll_region.tsx
+++ zk-money/src/ui-components/components/molecules/vertical_scroll_region/vertical_scroll_region.tsx
@@ -5,29 +5,44 @@ import {
 } from 'ui-components/components/atoms/vertical_scroll_bar/vertical_scroll_bar';
 import style from './vertical_scroll_region.module.css';
 
+const FADE_WIDTH = 30;
+
+function applyFade(elem: HTMLDivElement, viewHeight: number, contentHeight: number, scrollTop: number) {
+  const topFadeSize = Math.min(scrollTop, FADE_WIDTH);
+  const bottomFadeSize = Math.min(Math.max(contentHeight - (scrollTop + viewHeight), 0), FADE_WIDTH);
+  elem.style.mask =
+    elem.style.webkitMask = `linear-gradient(transparent 0%, white ${topFadeSize}px, white calc(100% - ${bottomFadeSize}px), transparent 100%)`;
+}
+
 export function VerticalScrollRegion(props: { children?: React.ReactNode }) {
   const rootRef = useRef<HTMLDivElement>(null);
-  const scrollRef = useRef<HTMLDivElement>(null);
+  const viewportRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
   const barRef = useRef<VerticalScrollBarHandle>(null);
   const [contentHeight, setContentHeight] = useState(0);
   const [viewHeight, setViewHeight] = useState(0);
   useEffect(() => {
     requestAnimationFrame(() => {
-      setContentHeight(contentRef.current?.offsetHeight ?? 0);
-      setViewHeight(rootRef.current?.offsetHeight ?? 0);
+      const contentHeight = contentRef.current?.offsetHeight ?? 0;
+      setContentHeight(contentHeight);
+      const viewHeight = rootRef.current?.offsetHeight ?? 0;
+      setViewHeight(viewHeight);
+      if (viewportRef.current) applyFade(viewportRef.current, viewHeight, contentHeight, 0);
     });
   }, []);
   const handleScrollTo = useCallback((scrollTop: number) => {
-    const elem = scrollRef.current;
+    const elem = viewportRef.current;
     if (elem) elem.scrollTop = scrollTop;
+    if (viewportRef.current) applyFade(viewportRef.current, viewHeight, contentHeight, scrollTop);
   }, []);
   const handleScroll = () => {
-    barRef.current?.setScroll(scrollRef.current?.scrollTop ?? 0);
+    const scrollTop = viewportRef.current?.scrollTop ?? 0;
+    barRef.current?.setScroll(scrollTop);
+    if (viewportRef.current) applyFade(viewportRef.current, viewHeight, contentHeight, scrollTop);
   };
   return (
     <div className={style.root} ref={rootRef}>
-      <div className={style.content} ref={scrollRef} onScroll={handleScroll}>
+      <div className={style.viewport} ref={viewportRef} onScroll={handleScroll}>
         <div ref={contentRef}>{props.children}</div>
       </div>
       <VerticalScrollBar ref={barRef} viewHeight={viewHeight} contentHeight={contentHeight} onDragTo={handleScrollTo} />
