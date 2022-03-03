b46e64e2c6551282d8faa8c4fc95d720c11a4ba5
Charlie Lye <karl.lye@gmail.com>
2022-03-03 17:42:57 +0000

    Initial commit.

--- blockchain/package.js
+++ blockchain/package.js
@@ -8,6 +8,3 @@ const { jest, devDependencies, ...pkg } = package;
 
 pkg.dependencies['@aztec/barretenberg'] = 'file:../../barretenberg.js/dest';
 writeFileSync('./dest/package.json', JSON.stringify(pkg, null, '  '));
-
-pkg.dependencies['@aztec/barretenberg'] = '^2.0.0';
-writeFileSync('./dest/package.npm.json', JSON.stringify(pkg, null, '  '));
