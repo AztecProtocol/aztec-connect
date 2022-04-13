1280bfe9a683aee041897b892434ef18617316ec
joss-aztec <94053499+joss-aztec@users.noreply.github.com>
2022-04-13 17:08:41 +0100

    Shield txs are owned by nonce 0, and need to be added and sorted into the other txs in order to show a shield's pending state in tx history.

--- zk-money/src/alt-model/event_utils.ts
+++ zk-money/src/alt-model/event_utils.ts
@@ -1,8 +1,19 @@
 import { AccountId, SdkEvent, AztecSdk } from '@aztec/sdk';
 
-export function listenAccountUpdated(sdk: AztecSdk, accountId: AccountId, func: () => void) {
+export function listenAccountUpdated(
+  sdk: AztecSdk,
+  accountId: AccountId,
+  func: () => void,
+  opts?: { includeNonce0?: boolean },
+) {
+  const nonce0Id = new AccountId(accountId.publicKey, 0);
   const handleUpdatedUserState = (otherAccountId: AccountId) => {
-    if (otherAccountId.equals(accountId)) func();
+    if (
+      otherAccountId.publicKey.equals(accountId.publicKey) ||
+      (opts?.includeNonce0 && otherAccountId.equals(nonce0Id))
+    ) {
+      func();
+    }
   };
   sdk.on(SdkEvent.UPDATED_USER_STATE, handleUpdatedUserState);
   return () => {
--- zk-money/src/alt-model/user_tx_hooks.ts
+++ zk-money/src/alt-model/user_tx_hooks.ts
@@ -1,8 +1,16 @@
-import type { UserTx } from '@aztec/sdk';
+import { AccountId, UserTx } from '@aztec/sdk';
 import { useEffect, useState } from 'react';
 import { useApp } from './app_context';
 import { listenAccountUpdated } from './event_utils';
 import { useSdk } from './top_level_context';
+
+const bySettled = (tx1: UserTx, tx2: UserTx) => {
+  if (tx1.settled && tx2.settled) return tx2.settled.getTime() - tx1.settled.getTime();
+  if (!tx1.settled && !tx2.settled) return 0;
+  if (!tx1.settled) return -1;
+  if (!tx2.settled) return 1;
+  return 0;
+};
 
 export function useUserTxs() {
   const sdk = useSdk();
@@ -10,9 +18,18 @@ export function useUserTxs() {
   const [txs, setTxs] = useState<UserTx[]>();
   useEffect(() => {
     if (sdk && accountId) {
-      const updateTxs = () => sdk.getUserTxs(accountId).then(setTxs);
+      const accountNonce0 = new AccountId(accountId.publicKey, 0);
+      const updateTxs = () =>
+        Promise.all([
+          sdk.getUserTxs(accountId),
+          // Shield txs will be found under nonce 0
+          sdk.getUserTxs(accountNonce0),
+        ]).then(([txs1, txs2]) => {
+          const txs = txs1.concat(txs2).sort(bySettled);
+          setTxs(txs);
+        });
       updateTxs();
-      return listenAccountUpdated(sdk, accountId, updateTxs);
+      return listenAccountUpdated(sdk, accountId, updateTxs, { includeNonce0: true });
     }
   }, [sdk, accountId]);
   return txs;
