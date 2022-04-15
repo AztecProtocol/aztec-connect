c4b6aeb97310a7b25fb48a2612403265f2b3f32b
joss-aztec <94053499+joss-aztec@users.noreply.github.com>
2022-04-15 10:28:44 +0100

    Display recipe name in defi tx history (#853)

--- zk-money/src/components/transaction_history/transaction_history_row/transaction_type_field.tsx
+++ zk-money/src/components/transaction_history/transaction_history_row/transaction_type_field.tsx
@@ -1,7 +1,9 @@
-import { ProofId, UserTx } from '@aztec/sdk';
+import { ProofId, UserDefiClaimTx, UserDefiTx, UserTx } from '@aztec/sdk';
 import sendIcon from 'images/tx_type_send_icon.svg';
 import defiIcon from 'images/tx_type_defi_icon.svg';
 import style from './transaction_type_field.module.scss';
+import { useDefiRecipes } from 'alt-model/top_level_context';
+import { exitingRecipeMatcher, recipeMatcher } from 'alt-model/defi/recipe_matchers';
 
 function getTxTypeLabel(tx: UserTx) {
   switch (tx.proofId) {
@@ -31,6 +33,11 @@ function getIconSrc(proofId: ProofId) {
       return defiIcon;
   }
 }
+function DefiRecipeName({ tx }: { tx: UserDefiTx | UserDefiClaimTx }) {
+  const recipes = useDefiRecipes();
+  const recipe = recipes?.find(recipeMatcher(tx.bridgeId)) ?? recipes?.find(exitingRecipeMatcher(tx.bridgeId));
+  return <>{recipe?.name}</>;
+}
 
 interface TransactionTypeFieldProps {
   tx: UserTx;
@@ -38,10 +45,14 @@ interface TransactionTypeFieldProps {
 
 export function TransactionTypeField({ tx }: TransactionTypeFieldProps) {
   const iconSrc = getIconSrc(tx.proofId);
+  const isDefi = tx.proofId === ProofId.DEFI_CLAIM || tx.proofId === ProofId.DEFI_DEPOSIT;
   return (
     <div className={style.root}>
-      <div className={style.label}>{getTxTypeLabel(tx)}</div>
-      {iconSrc && <img alt="" src={iconSrc} />}
+      <div className={style.top}>
+        <div className={style.label}>{getTxTypeLabel(tx)}</div>
+        {iconSrc && <img alt="" src={iconSrc} />}
+      </div>
+      <div className={style.bottom}>{isDefi && <DefiRecipeName tx={tx} />}</div>
     </div>
   );
 }
