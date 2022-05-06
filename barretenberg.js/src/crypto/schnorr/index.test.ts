be9bf112ad390e4345592deb0ca221acfb0998ef
Zachary James Williamson <blorktronics@gmail.com>
2022-05-06 14:53:30 +0100

    Schnorr signature fixes, HMAC and Schnorr multisig (#881)
    * added HMAC implementation conforming to RFC4231
    
    crypto::schnorr uses HMAC to derive parameter k instead of randomness
    
    crypto::schnorr uses a pedersen hash of (r.x | pubkey.x | pubkey.y) when deriving challenge instead of just r.x
    
    stdlib::schnorr verifier updated to conform to above change
    
    * compile fixes
    
    * fixed compiler errors and join split tests
    
    join-split tests were not passing correct pubkey into sign_join_split_tx
    
    * fixed compiler errors
    
    * added SpeedyMuSig implementation
    
    SpeedyMuSig is a 2-round Schnorr multisig based on MuSig
    
    paper at https://eprint.iacr.org/2021
    
    * upgraded wasi version to be compatible with c++20 concepts
    
    * reverted wasi-sdk to v12
    
    * compiler fixes
    
    * added support for schnorr multisig, fixed tests
    
    added barretenberg.js interface for schnorr multisigs with tests
    
    * Update Dockerfile.wasm-linux-clang
    
    typo fix
    
    * Update Dockerfile.wasm-linux-clang
    
    typo fix 2
    
    * Update Dockerfile.wasm-linux-clang
    
    typo fix 3
    
    * Update Dockerfile.wasm-linux-clang
    
    * Update wasm-linux-clang.cmake
    
    * updated schnorr spec in `markdown`
    
    * removed HMAC from schnorr multisig
    added proof of ownership to multisig protocol
    updated multisig comments
    
    * updated multisig tests
    
    * round 2 returns -1 if public key validation fails
    
    * typo posession -> possession
    
    * warn about invalid aggregate key
    
    * add aggregate key check in combine_sigs
    
    * remove unused data and comments
    
    * remove git submodules incorrectly added
    
    Co-authored-by: adr1anh <adrian.hamelink@gmail.com>

--- barretenberg.js/src/crypto/schnorr/index.test.ts
+++ barretenberg.js/src/crypto/schnorr/index.test.ts
@@ -1,27 +1,66 @@
 import { Schnorr } from './index';
 import { TextEncoder } from 'util';
 import { BarretenbergWasm } from '../../wasm';
+import { Grumpkin } from '../../ecc/grumpkin';
 
 describe('schnorr', () => {
   let barretenberg!: BarretenbergWasm;
   let schnorr!: Schnorr;
+  let grumpkin!: Grumpkin;
 
   beforeAll(async () => {
     barretenberg = new BarretenbergWasm();
     await barretenberg.init();
     schnorr = new Schnorr(barretenberg);
+    grumpkin = new Grumpkin(barretenberg);
   });
 
   it('should verify signature', async () => {
     // prettier-ignore
     const pk = Buffer.from([
-      0x0b, 0x9b, 0x3a, 0xde, 0xe6, 0xb3, 0xd8, 0x1b, 0x28, 0xa0, 0x88, 0x6b, 0x2a, 0x84, 0x15, 0xc7,
-      0xda, 0x31, 0x29, 0x1a, 0x5e, 0x96, 0xbb, 0x7a, 0x56, 0x63, 0x9e, 0x17, 0x7d, 0x30, 0x1b, 0xeb ]);
-
+      0x0b, 0x9b, 0x3a, 0xde, 0xe6, 0xb3, 0xd8, 0x1b, 0x28, 0xa0, 0x88, 0x6b, 0x2a, 0x84, 0x15, 0xc7, 0xda, 0x31, 0x29,
+      0x1a, 0x5e, 0x96, 0xbb, 0x7a, 0x56, 0x63, 0x9e, 0x17, 0x7d, 0x30, 0x1b, 0xeb,
+    ]);
     const pubKey = schnorr.computePublicKey(pk);
     const msg = new TextEncoder().encode('The quick brown dog jumped over the lazy fox.');
     const signature = schnorr.constructSignature(msg, pk);
     const verified = schnorr.verifySignature(msg, pubKey, signature);
+
+    expect(verified).toBe(true);
+  });
+
+  it('should create + verify multi signature', async () => {
+    const pks: Buffer[] = [];
+    const pubKeys: Buffer[] = [];
+    const roundOnePublicOutputs: Buffer[] = [];
+    const roundOnePrivateOutputs: Buffer[] = [];
+    const roundTwoOutputs: Buffer[] = [];
+    const numSigners = 7;
+    const msg = new TextEncoder().encode('The quick brown dog jumped over the lazy fox.');
+
+    // set up multisig accounts
+    for (let i = 0; i < numSigners; ++i) {
+      const pk = grumpkin.getRandomFr();
+      pks.push(pk);
+      pubKeys.push(schnorr.multiSigComputePublicKey(pk));
+    }
+
+    // round one
+    for (let i = 0; i < numSigners; ++i) {
+        const { publicOutput, privateOutput } = schnorr.multiSigRoundOne();
+      roundOnePublicOutputs.push(publicOutput);
+      roundOnePrivateOutputs.push(privateOutput);
+    }
+
+    // round two
+    for (let i = 0; i < numSigners; ++i) {
+      roundTwoOutputs.push(schnorr.multiSigRoundTwo(msg, pks[i], roundOnePrivateOutputs[i], pubKeys, roundOnePublicOutputs));
+    }
+
+    // generate signature
+    const signature = schnorr.multiSigCombineSignatures(msg, pubKeys, roundOnePublicOutputs, roundTwoOutputs);
+
+    const verified = schnorr.verifySignature(msg, schnorr.multiSigValidateAndCombinePublicKeys(pubKeys), signature);
 
     expect(verified).toBe(true);
   });
--- barretenberg.js/src/crypto/schnorr/index.ts
+++ barretenberg.js/src/crypto/schnorr/index.ts
@@ -1,6 +1,8 @@
 import { BarretenbergWasm } from '../../wasm';
 import { SchnorrSignature } from './signature';
 
+import { serializeBufferArrayToVector } from '../../serialize';
+
 export * from './signature';
 
 export class Schnorr {
@@ -25,5 +27,85 @@ export class Schnorr {
     this.wasm.transferToHeap(sig.e(), 96);
     this.wasm.transferToHeap(msg, 128);
     return this.wasm.call('verify_signature', 128, msg.length, 0, 64, 96) ? true : false;
+  }
+
+  public multiSigComputePublicKey(pk: Uint8Array) {
+    this.wasm.transferToHeap(pk, 128);
+    this.wasm.call('multisig_create_multisig_public_key', 128, 0);
+    return Buffer.from(this.wasm.sliceMemory(0, 128));
+  }
+
+  public multiSigValidateAndCombinePublicKeys(pubKeys: Buffer[]) {
+    const buffer = serializeBufferArrayToVector(pubKeys);
+    this.wasm.transferToHeap(buffer, 64);
+    this.wasm.call('multisig_validate_and_combine_signer_pubkeys', 64, 0);
+    return Buffer.from(this.wasm.sliceMemory(0, 64));
+  }
+
+  public multiSigRoundOne() {
+    this.wasm.call('multisig_construct_signature_round_1', 0, 128);
+
+    return {
+      publicOutput: Buffer.from(this.wasm.sliceMemory(0, 128)),
+      privateOutput: Buffer.from(this.wasm.sliceMemory(128, 192)),
+    };
+  }
+
+  public multiSigRoundTwo(
+    msg: Uint8Array,
+    pk: Uint8Array,
+    signerrRoundOnePrivateOutput: Buffer,
+    pubKeys: Buffer[],
+    roundOnePublicOutputs: Buffer[],
+  ) {
+    const pubKeysBuffer = serializeBufferArrayToVector(pubKeys);
+    const roundOneOutputsBuffer = serializeBufferArrayToVector(roundOnePublicOutputs);
+    let msgPtr = 32;
+
+    this.wasm.transferToHeap(msg, msgPtr);
+    let pkPtr = msgPtr + msg.length;
+    this.wasm.transferToHeap(pk, pkPtr);
+    let roundOnePrivatePtr = pkPtr + 32;
+    this.wasm.transferToHeap(signerrRoundOnePrivateOutput, roundOnePrivatePtr);
+    let pubKeysPtr = roundOnePrivatePtr + 64;
+    this.wasm.transferToHeap(pubKeysBuffer, pubKeysPtr);
+    let roundOnePtr = pubKeysPtr + pubKeysBuffer.length;
+    this.wasm.transferToHeap(roundOneOutputsBuffer, roundOnePtr);
+
+    this.wasm.call(
+      'multisig_construct_signature_round_2',
+      msgPtr,
+      msg.length,
+      pkPtr,
+      roundOnePrivatePtr,
+      pubKeysPtr,
+      roundOnePtr,
+      0,
+    );
+    return Buffer.from(this.wasm.sliceMemory(0, 32));
+  }
+
+  public multiSigCombineSignatures(
+    msg: Uint8Array,
+    pubKeys: Buffer[],
+    roundOneOutputs: Buffer[],
+    roundTwoOutputs: Buffer[],
+  ) {
+    const pubKeysBuffer = serializeBufferArrayToVector(pubKeys);
+    const roundOneOutputsBuffer = serializeBufferArrayToVector(roundOneOutputs);
+    const roundTwoOutputsBuffer = serializeBufferArrayToVector(roundTwoOutputs);
+
+    let msgPtr = 64;
+
+    this.wasm.transferToHeap(msg, msgPtr);
+    let pubKeysPtr = msgPtr + msg.length;
+    this.wasm.transferToHeap(pubKeysBuffer, pubKeysPtr);
+    let roundOnePtr = pubKeysPtr + pubKeysBuffer.length;
+    this.wasm.transferToHeap(roundOneOutputsBuffer, roundOnePtr);
+    let roundTwoPtr = roundOnePtr + roundOneOutputsBuffer.length;
+    this.wasm.transferToHeap(roundTwoOutputsBuffer, roundTwoPtr);
+
+    this.wasm.call('multisig_combine_signatures', msgPtr, msg.length, pubKeysPtr, roundOnePtr, roundTwoPtr, 0, 32);
+    return new SchnorrSignature(Buffer.from(this.wasm.sliceMemory(0, 64)));
   }
 }
