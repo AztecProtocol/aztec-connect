236eda7b0f3098e06ac5a84fae30fd90c4abc0ac
Charlie Lye <karl.lye@gmail.com>
2022-05-12 22:13:32 +0000

    Add symlinks for hummus/explorer prod deploys. Advance earliestBlock in falafel.

--- blockchain/src/contracts/rollup_processor/rollup_processor.ts
+++ blockchain/src/contracts/rollup_processor/rollup_processor.ts
@@ -311,7 +311,7 @@ export class RollupProcessor {
     const net = await this.provider.getNetwork();
     switch (net.chainId) {
       case 1:
-        return { earliestBlock: 11967192, chunk: 100000, offchainSearchLead: 6 * 60 * 24 };
+        return { earliestBlock: 14728000, chunk: 100000, offchainSearchLead: 6 * 60 * 24 };
       case 0xa57ec:
       case 0xe2e:
         return { earliestBlock: 14728000, chunk: 10, offchainSearchLead: 10 };
