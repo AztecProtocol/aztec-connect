56353b361141ded3e6a454c50311b9a454079741
Joe Andrews <joe@creditmint.io>
2022-06-14 17:55:41 +0100

    fix the lambda function (#1080)

--- zk-money/lambda/src/index.ts
+++ zk-money/lambda/src/index.ts
@@ -13,48 +13,50 @@ interface ZkParams {
   alias?: string;
 }
 
-exports.main = async function (event: any) {
-  const { request } = event.Records[0].cf;
+exports.main = async function (event: any, ctx: any, callback: any) {
+  const { request, response } = event.Records[0].cf;
   const queryParams = new URLSearchParams(request.querystring) as URLSearchParams;
   const queryParamsObj = paramsToObject(queryParams.entries()) as ZkParams;
   const alias = queryParamsObj.alias;
+  const uri = request.uri;
+  const lambdaPaths = ['/', '/index.html'];
 
-  const response = {
-    status: '200',
-    statusDescription: 'OK',
-  };
-  let oldBody = await (await fetch('https://zk.money/index.html')).text();
+  response.status = '200';
+  response.statusDescription = 'OK';
 
-  if (alias) {
-    oldBody = oldBody.replace(
+  let oldBody = await (await fetch('http://zk.money-aztec-connect-dev.s3-website.eu-west-2.amazonaws.com')).text();
+
+  if (alias && lambdaPaths.indexOf(uri) > -1) {
+    response.body = oldBody.replace(
       '$IMAGE_CONTENT',
       `https://res.cloudinary.com/df4pltas6/image/upload/c_scale,w_1459/g_south_east,l_e_colorize,co_white,l_text:lato_80:@${alias},x_170,y_270/v1615319371/Share_image_3_uo7zrx.png`,
     );
-    oldBody = oldBody.replace(
-      '$IMAGE_CONTENT',
-      `https://res.cloudinary.com/df4pltas6/image/upload/c_scale,w_1459/g_south_east,l_e_colorize,co_white,l_text:lato_80:@${alias},x_170,y_270/v1615319371/Share_image_3_uo7zrx.png`,
-    );
-    oldBody = oldBody.replace(
+    response.body = response.body.replace(
       '$TEXT_CONTENT',
       `Checkout zk.money by @aztecnetwork. Private DeFi is here. Send me crypto privately @${alias} 🕵️.`,
     );
-    return {
-      ...response,
-      body: oldBody,
-    };
-  } else {
-    oldBody = oldBody.replace(
+    response.headers['content-type'] = [
+      {
+        key: 'Content-Type',
+        value: 'text/html; charset=utf-8',
+      },
+    ];
+  } else if (lambdaPaths.indexOf(uri) > -1) {
+    response.body = oldBody.replace(
       '$IMAGE_CONTENT',
       `https://res.cloudinary.com/df4pltas6/image/upload/c_scale,w_1459/v1615319371/Share_image_3_uo7zrx.png`,
     );
-    oldBody = oldBody.replace(
-      '$IMAGE_CONTENT',
-      `https://res.cloudinary.com/df4pltas6/image/upload/c_scale,w_1459/v1615319371/Share_image_3_uo7zrx.png`,
+    response.body = response.body.replace(
+      '$TEXT_CONTENT',
+      `Checkout zk.money by @aztecnetwork. Private DeFi is here. 🕵️.`,
     );
-    oldBody = oldBody.replace('$TEXT_CONTENT', `Checkout zk.money by @aztecnetwork. Private DeFi is here. 🕵️.`);
-    return {
-      ...response,
-      body: oldBody,
-    };
+
+    response.headers['content-type'] = [
+      {
+        key: 'Content-Type',
+        value: 'text/html; charset=utf-8',
+      },
+    ];
   }
+  callback(null, response);
 };
