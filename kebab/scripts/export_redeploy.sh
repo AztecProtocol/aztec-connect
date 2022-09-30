839e7e7944a20d463515bae7aafd77d8046a211f
spypsy <spypsy@users.noreply.github.com>
2022-09-30 18:18:25 +0100

    Force contract redeploy (dev) (#1568)
    * force contract redeploy + add port to kebab health check
    
    * undo port specification for healthcheck

--- kebab/scripts/export_redeploy.sh
+++ kebab/scripts/export_redeploy.sh
@@ -5,7 +5,7 @@ if [ -z "$BASH_ENV" ]; then
   BASH_ENV=$(mktemp)
 fi
 
-echo export TF_VAR_REDEPLOY=1 >> $BASH_ENV
+echo export TF_VAR_REDEPLOY=2 >> $BASH_ENV
 cat $BASH_ENV
 
 # Having written the variables to $BASH_ENV, we now want to set them in this shell context.
