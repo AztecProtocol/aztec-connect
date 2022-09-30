13303ce77a1ece4e46e6bdef3766b4fc5dc3b744
PhilWindle <60546371+PhilWindle@users.noreply.github.com>
2022-09-30 13:20:55 +0100

    Fixed scripting (#1567)

--- kebab/scripts/export_redeploy.sh
+++ kebab/scripts/export_redeploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
-export TF_VAR_REDEPLOY=1
+if [ -z "$BASH_ENV" ]; then
+  BASH_ENV=$(mktemp)
+fi
+
+echo export TF_VAR_REDEPLOY=1 >> $BASH_ENV
+cat $BASH_ENV
+
+# Having written the variables to $BASH_ENV, we now want to set them in this shell context.
+source $BASH_ENV
