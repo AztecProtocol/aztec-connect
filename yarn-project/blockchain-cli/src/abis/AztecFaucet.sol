9a13ff64c3170655e178e1aa2375d5aa9fb9d94a
Maddiaa <47148561+cheethas@users.noreply.github.com>
2023-01-04 10:05:53 +0000

    [Tech-Debt] Blockchain CLI refactor (#2000)
    * init: blockchain-cli
    
    * tweak: config items
    
    * feat: finish extraction
    
    * fix: yarn
    
    * revert: accidental edit to block-server
    
    * fix: dockerfile
    
    * docs: readme

---
+++ yarn-project/blockchain-cli/src/abis/AztecFaucet.sol
@@ -0,0 +1,1 @@
+../../../../contracts/out/AztecFaucet.sol
--- yarn-project/blockchain/src/contracts/index.ts
+++ yarn-project/blockchain/src/contracts/index.ts
@@ -4,3 +4,4 @@ export * from './fee_distributor/index.js';
 export * from './price_feed/index.js';
 export * from './rollup_processor/index.js';
 export * from './bridge_data_provider/index.js';
+export * from './decode_error.js';
