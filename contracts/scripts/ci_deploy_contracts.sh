e96d24ff964d6de05241b1b6849f2fb53cc0e993
Maddiaa <47148561+cheethas@users.noreply.github.com>
2023-01-04 12:40:27 +0000

    fix: ci faucet controller (#2082)
    * fix: faucet controller wrong
    
    * fix: swap

--- contracts/scripts/ci_deploy_contracts.sh
+++ contracts/scripts/ci_deploy_contracts.sh
@@ -7,11 +7,13 @@ case $VERSION_TAG in
     export ETHEREUM_HOST=$TF_VAR_TEST_NET_RPC_URL
     export PRIVATE_KEY=$TF_VAR_TEST_NET_ROOT_PRIVATE_KEY
     export DEPLOYER_ADDRESS=$TF_VAR_TEST_NET_DEPLOYER_ADDRESS
+    export FAUCET_CONTROLLER=$TF_VAR_TEST_NET_FAUCET_OPERATOR_ADDRESS
     ;;
   dev)
     export ETHEREUM_HOST=$TF_VAR_DEV_NET_RPC_URL
     export PRIVATE_KEY=$TF_VAR_DEV_NET_ROOT_PRIVATE_KEY
     export DEPLOYER_ADDRESS=$TF_VAR_DEV_NET_DEPLOYER_ADDRESS
+    export FAUCET_CONTROLLER=$TF_VAR_DEV_NET_FAUCET_OPERATOR_ADDRESS
     ;;
   *)
     echo "No configuration for VERSION_TAG=$VERSION_TAG, skipping contract deployment."
@@ -26,17 +28,11 @@ if [ -z "$LAST_COMMIT" ]; then
 elif changed $LAST_COMMIT "contracts/deploy/$VERSION_TAG"; then
   echo "Redeploying contracts..."
 
-  # TODO: Get rid of tenderly stuff. They failed us...
-  if [[ $ETHEREUM_HOST == *"tenderly"* ]]; then
-    # If on a tenderly fork, increase the balance of the Deployer, faucet and rollup provider, 24 is the number of 0s of eth to provide the account
-    ./scripts/tenderly_increase_balance.sh $ETHEREUM_HOST 24 "\"$DEPLOYER_ADDRESS\",\"$TF_VAR_FAUCET_OPERATOR_ADDRESS\""
-  fi
-
   mkdir -p serve
   # Contract addresses will be mounted in the serve directory
   docker run \
     -v $(pwd)/serve:/usr/src/contracts/serve \
-    -e ETHEREUM_HOST=$ETHEREUM_HOST -e PRIVATE_KEY=$PRIVATE_KEY -e UPGRADE=true -e network=None -e simulateAdmin=false \
+    -e ETHEREUM_HOST=$ETHEREUM_HOST -e PRIVATE_KEY=$PRIVATE_KEY -e FAUCET_CONTROLLER=$FAUCET_CONTROLLER -e UPGRADE=true -e network=None -e simulateAdmin=false \
     278380418400.dkr.ecr.eu-west-2.amazonaws.com/contracts:$COMMIT_HASH
 
   # Write the contract addresses as terraform variables
