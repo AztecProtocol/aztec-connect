cdc8e8c26cf73959bf48f12d46280bb5bff5cc3a
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2023-01-19 15:28:55 +0000

    Lh/2262 aztec dev cli add data provider script (#2264)
    * feat: Add data provider fetch command to aztec-dev-cli
    
    * fix: bignumber return value
    
    * fix: delete data provider json
    
    * fix: symbolic linked data provider json

--- yarn-project/aztec-dev-cli/src/abis/index.ts
+++ yarn-project/aztec-dev-cli/src/abis/index.ts
@@ -2,3 +2,4 @@ export { default as ElementBridge } from './ElementBridge.sol/ElementBridge.json
 export { default as IVault } from './IVault.sol/IVault.json' assert { type: 'json' };
 export { default as RollupProcessor } from './RollupProcessorV2.sol/RollupProcessorV2.json' assert { type: 'json' };
 export { default as AztecFaucetJson } from './AztecFaucet.sol/AztecFaucet.json' assert { type: 'json' };
+export { default as DataProviderJson} from './DataProvider.sol/DataProvider.json' assert { type: 'json' };
