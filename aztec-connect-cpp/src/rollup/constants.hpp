3d4558d90bf5a2c1d9e227285e09f7f8d1dda95c
Innokentii Sennovskii <isennovskiy@gmail.com>
2024-02-15 16:57:38 +0000

    fix: Fixes for 3 issues (#62)
    Co-authored-by: Maddiaa <47148561+Maddiaa0@users.noreply.github.com>

--- aztec-connect-cpp/src/rollup/constants.hpp
+++ aztec-connect-cpp/src/rollup/constants.hpp
@@ -39,9 +39,9 @@ constexpr bool is_circuit_change_expected = 0;
 constexpr uint32_t ACCOUNT = 23967;
 constexpr uint32_t JOIN_SPLIT = 64047;
 constexpr uint32_t CLAIM = 23050;
-constexpr uint32_t ROLLUP = 1173221;
-constexpr uint32_t ROOT_ROLLUP = 5481327;
-constexpr uint32_t ROOT_VERIFIER = 7435892;
+constexpr uint32_t ROLLUP = 1167809;
+constexpr uint32_t ROOT_ROLLUP = 5466707;
+constexpr uint32_t ROOT_VERIFIER = 7628270;
 }; // namespace circuit_gate_count
 
 namespace circuit_gate_next_power_of_two {
@@ -62,11 +62,11 @@ namespace circuit_vk_hash {
 constexpr auto ACCOUNT = uint256_t(0xcd6d70c733eaf823, 0x6505d3402817ad3d, 0xbf9e2b6a262589cf, 0xafcc546b55cc45e3);
 constexpr auto JOIN_SPLIT = uint256_t(0xb23c7772f47bc823, 0x5493625d4f08603c, 0x21ac50a5929576f9, 0xb7b3113c131460e5);
 constexpr auto CLAIM = uint256_t(0xa753ce523719749e, 0x80216aff7f8bc9ce, 0xa9b0f69bbd24ac33, 0xae17c5fb7d488138);
-constexpr auto ROLLUP = uint256_t(0x5f2f6590e5553f19, 0x62c287e01b897621, 0xf32d03437085e2d, 0x567b0be24dc99966);
-constexpr auto ROOT_ROLLUP = uint256_t(0x64e5e03cf9534ed6, 0x7fdc871935b9e4fe, 0xd2b81e990cc15f3d, 0x47f00f76d92e5e4d);
-;
+constexpr auto ROLLUP = uint256_t(0x47427863b042e198, 0xbcfaeb63d9e263e, 0x405c66379df643d0, 0x11a2cb6aef44a77d);
+constexpr auto ROOT_ROLLUP = uint256_t(0x3f9f4b9944097e45, 0x20279b5f76e6ec69, 0xd7a31ace33aaed41, 0x49f209bdff64342c);
 constexpr auto ROOT_VERIFIER =
-    uint256_t(0xb4349747ae6ea507, 0xfaafa0f2e384c984, 0x9325870bcc594daf, 0x50163a2572c67363);
+    uint256_t(0x7de149243dd52594, 0x1c40fbda00798466, 0x8afc5663ee50a18c, 0xa1c5c44397212706);
+
 }; // namespace circuit_vk_hash
 
 namespace ProofIds {
